6034317fe7ffd0679170050f5cc251ca67fae151
Kareem Francis <francis.kareem+git@gmail.com>
2014-06-08 11:25:42 -0400

    added Amenities model

---
+++ server/models/amenitiesModel.js
@@ -0,0 +1,19 @@
+'use strict';
+
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema,
+    _ = require('lodash'),
+    base = require('./baseModel');
+
+var properties = _.extend(base.properties, {
+    name: String,
+    type: String, // Food, Beverage
+    description: String,
+    hours: String
+    specialities: [String] //Vegan, Kosher, etc
+    location: String,
+    geoLocation: {type: [Number], index: '2d'}
+});
+var AmenitiesSchema = new Schema(properties;
+
+mongoose.model('Amenities', AmenitiesSchema);
