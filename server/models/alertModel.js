d2e51c676406b958397d1ce2dd0d4db25814cc22
Kareem Francis <francis.kareem+git@gmail.com>
2014-06-10 23:53:44 -0400

    added missing .js ext to alertModel

---
+++ server/models/alertModel.js
@@ -0,0 +1,18 @@
+'use strict';
+
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema,
+    _ = require('lodash'),
+    base = require('./baseModel');
+
+var properties = _.extend(base.properties, {
+    description: String,
+    startDateTime: Date,
+    endDateTime: Date,
+    isReccuring: Boolean,
+    location: String,
+    geoLocation: {type: [Number], index: '2d'}
+});
+var AlertSchema = new Schema(properties);
+
+mongoose.model('Alert', AlertSchema);
