a91463f6dd2f6c07044d6c9b4876ee9199410853
Kareem Francis <francis.kareem+git@gmail.com>
2014-06-11 22:43:47 -0400

    using angular-google-maps to render the map

--- public/js/app.js
+++ public/js/app.js
@@ -3,5 +3,11 @@
 var app = angular.module('app', ['ngResource', 'google-maps']);
 
 app.controller('MapController', ['$scope', function($scope){
-
+    $scope.mapOptions = {
+        center: {
+            latitude: 40.6880492,
+            longitude: -74.0188415
+        },
+        zoom: 16
+    };
 }])
