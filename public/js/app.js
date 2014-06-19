69d0ae7ebfb29f0d13cc94332f65d7f4b5ec6be0
Kareem Francis <francis.kareem+git@gmail.com>
2014-06-19 00:09:09 -0400

    Moved Geocoder service to separate file

--- public/js/app.js
+++ public/js/app.js
@@ -11,47 +11,7 @@ var app = angular.module('app', [
 
 
 
-services.factory('Geocoder', ['$q', function($q){
-    var geocoder = new google.maps.Geocoder();
-    var southWest = new google.maps.LatLng(40.68391889999999, -74.02667689999998);
-    var northEast = new google.maps.LatLng(40.69379199999999, -74.01198210000001);
-    var bounds = new google.maps.LatLngBounds(southWest, northEast);
-    var request = {
-        bounds: bounds
-    }
 
-    var exec = function(request, deferred){
-        geocoder.geocode(request, function(results, status) {
-            console.log('geocoder results', results, 'status', status);
-            // if (status == google.maps.GeocoderStatus.OK) {}
-            deferred.resolve({
-                results: results,
-                status: status
-            });
-        });
-    };
-
-    return {
-        lookup: function(location){
-            // attempt to geocode location
-            var deferred = $q.defer();
-            request.address = location;
-
-            exec(request, deferred);
-
-            return deferred.promise;
-        },
-        reverseLookup: function(geoLocation){
-            // extract location from geocode
-            var deferred = $q.defer();
-            request.location = new google.maps.LatLng(geoLocation[0], geoLocation[1]);
-
-            exec(request, deferred);
-
-            return deferred.promise;
-        }
-    };
-}]);
 
 controllers.controller('MapController', ['$scope', 'Events', function($scope, Events){
     // TODO: only grab relevant content
