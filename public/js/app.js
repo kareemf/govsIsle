b083799f4a804a4f5a1e031399538372726106d3
Kareem Francis <francis.kareem+git@gmail.com>
2014-06-22 12:33:22 -0400

    Redirecting to home state if URL state is undefined

--- public/js/app.js
+++ public/js/app.js
@@ -11,17 +11,20 @@ var app = angular.module('app', [
     'ui.router']);
 
 app.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
-  $stateProvider
-    .state('home', {
-        url: '/',
-        templateUrl: 'templates/home.html'
-    })
-    .state('login', {
-        url: '/login',
-        templateUrl: 'templates/login.html'
-    })
-    .state('register', {
-        url: '/register',
-        templateUrl: 'templates/register.html'
-    })
+    $stateProvider
+        .state('home', {
+            url: '/',
+            templateUrl: 'templates/home.html'
+        })
+        .state('login', {
+            url: '/login',
+            templateUrl: 'templates/login.html'
+        })
+        .state('register', {
+            url: '/register',
+            templateUrl: 'templates/register.html'
+        });
+
+    $urlRouterProvider.otherwise('/');
+
 }])
