47a157da62019c0d31099ecba7096a5e9606b164
Kareem Francis <francis.kareem+git@gmail.com>
2014-06-26 21:15:22 -0400

    Merged maps2.js back into map.js. Deleted maps2.js

--- public/js/controllers/map.js
+++ public/js/controllers/map.js
@@ -1,67 +1,111 @@
 'use strict';
 
+function initCall() {
+  console.log('Google maps api initialized.');
+  angular.bootstrap(document.getElementById('map'), ['app']);
+}
+
 var controllers = angular.module('app.controllers');
 
-controllers.controller('MapController', ['$scope', 'Events', function($scope, Events){
+controllers.controller('MapController2', ['$scope', 'Events', function ($scope, Events) {
+    console.log('Google maps controller.');
+
+    /* $scope.myMap auto-populated with google map object */
+    $scope.isEditMode = true;
+
+    $scope.newMarkers = [];
+
+    $scope.existingMarkers = [];
+
     // TODO: only grab relevant content
     // TODO: only one info window for whole app
     $scope.events = Events.query(function(events){
         console.log('events', events);
+        events.forEach(function(event){
+            var position = new google.maps.LatLng(event.geoLocation[0], event.geoLocation[1]);
+            var marker = new google.maps.Marker({
+                map: $scope.myMap,
+                position: position,
+                draggable: false
+            });
+
+            // console.log('existing event', event, 'marker', marker);
+            $scope.existingMarkers.push(marker);
+        });
     });
 
-    //will be automatically populated b/c of 'controls' property on map DOM element
-    $scope.map = {};
-
-    var infoWindow = new google.maps.InfoWindow();
-    $scope.mapParams = {
-        options: {
-            streetViewControl: true,
-            panControl: true,
-            maxZoom: 20,
-            minZoom: 3
-        },
-        center: {
-            latitude: 40.6880492,
-            longitude: -74.0188415
-        },
-        zoom: 16,
-        events: {
-            rightclick: function(map, eventName, args){
-                console.log('rightclick', map, eventName, args);
-
-                if(!$scope.isEditMode){
-                    console.log('not isEditMode');
-                    return;
-                }
-
-                // TODO: check user's permissions first
-                var clickLocation = args[0].latLng;
-
-                var marker = new google.maps.Marker({
-                    map: map,
-                    position: new google.maps.LatLng(clickLocation.k, clickLocation.A),
-                    draggable: true
-                });
-
-                google.maps.event.addListener(marker, 'click', function() {
-                    //there is only one infoWindow, which then gets moved around.
-                    infoWindow.open(map, marker);
-                });
-
-                //inform other controllers of the markers creation
-                $scope.$broadcast('MARKER_ADDED_EVENT', {
-                    marker: marker,
-                    infoWindow: infoWindow
-                });
-            }
-        }
+    $scope.mapOptions = {
+        center: new google.maps.LatLng(40.6880492, -74.0188415),
+        streetViewControl: true,
+        panControl: true,
+        zoom: 15,
+        maxZoom: 20,
+        minZoom: 14,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
     };
 
-    $scope.isEditMode = false;
+    $scope.mapEvents = {
+        'map-rightclick': 'addNewMarker($event, $params)',
+    };
 
-    $scope.$on('MARKER_CAN_BE_EDITED_EVENT', function(event, args){
-        console.log('responding to MARKER_CAN_BE_EDITED_EVENT in MapController');
+    $scope.markerEvents = {
+        'map-click': 'openMarkerInfo(marker, events[$index])',
+        'map-rightclick': 'editMarker(marker, events[$index])',
+        'map-dragend': 'updateGeolocationAfterDrag(events[$index], marker)'
+    };
 
-        $scope.$broadcast('MARKER_CAN_BE_EDITED_EVENT'+'!', args);
-    });
-}]);
+    $scope.newMarkerEvents = {
+        'map-click': 'openMarkerInfo(marker, event)',
+        'map-rightclick': 'editMarker(marker, event)',
+        'map-dragend': 'updateGeolocationAfterDrag(event, marker)'
+    };
+
+    $scope.addNewMarker = function ($event, $params) {
+        console.log('rightclick', $event, $params);
+
+        if(!$scope.isEditMode){
+            console.log('not isEditMode');
+            return;
+        }
+
+        // TODO: check user's permissions first
+        var marker = new google.maps.Marker({
+            map: $scope.myMap,
+            position: $params[0].latLng,
+            draggable: true
+        });
+
+        $scope.newMarkers.push(marker);
+    };
+
+    $scope.openMarkerInfo = function (marker, event) {
+        console.log('openMarkerInfo marker', marker, 'event', event );
+
+        $scope.currentMarker = marker;
+        $scope.currentEvent = event;
+        $scope.myInfoWindow.open($scope.myMap, marker);
+    };
+
+    $scope.editMarker = function(marker, event){
+        console.log('editting marker', marker, 'event', event);
+
+        $scope.$broadcast('MARKER_CAN_BE_EDITED_EVENT', {
+            marker: marker,
+            event: event
+        });
+    };
+
+    //TODO: do not duplicate. see BaseEventController
+    var getMarkerGeoLocation = $scope.getMarkerGeoLocation = function(marker){
+        var position = marker.position
+        return [position.k, position.A];
+    };
+
+    $scope.updateGeolocationAfterDrag = function(event, marker){
+        console.log('updating event position');
+
+        var geoLocation = getMarkerGeoLocation(marker);
+        event.geoLocation = geoLocation;
+    };
+
+  }]);
