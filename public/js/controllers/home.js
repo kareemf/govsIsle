322f6d14577cb659e5caabd756c0228e6e7758e8
Kareem Francis <francis.kareem+git@gmail.com>
2014-06-28 12:39:57 -0400

    Added home controller

---
+++ public/js/controllers/home.js
@@ -0,0 +1,8 @@
+'use strict';
+
+var controllers = angular.module('app.controllers');
+
+controllers.controller('HomeController', ['$scope', '$stateParams', function($scope, $stateParams){
+    console.log('In HomeController');
+
+}]);
