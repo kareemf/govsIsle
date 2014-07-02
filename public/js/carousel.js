f079ae2d6ed4431ff5a793cfcd20eb43b5c2da23
Lonique Alexander <l.w.alexander@hotmail.com>
2014-07-02 11:32:45 -0400

    carousel fixes

---
+++ public/js/carousel.js
@@ -0,0 +1,26 @@
+$(document).ready( function() {
+
+  $('#home-carousel').carousel({
+    interval: 2000
+  });
+
+  var clickEvent = false;
+  $('#home-carousel').on('click', '.nav a', function() {
+    clickEvent = true;
+    $('.nav li').removeClass('carousel-info');
+    $(this).parent().addClass('carousel-info');
+
+  }).on('slid.bs.carousel', function(e) {
+    if(!clickEvent) {
+
+      var count = $('#home-carousel .nav').children().length -1;
+      var current = $('#home-carousel .nav li.carousel-info');
+      current.removeClass('carousel-info').next().addClass('carousel-info');
+      var id = parseInt(current.data('slide-to'));
+      if(count == id) {
+        $('#home-carousel .nav li').first().addClass('carousel-info');
+      }
+    }
+    clickEvent = false;
+  });
+});
