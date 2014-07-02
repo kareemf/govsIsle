5af46deb84d67ca72f0bce078f4afa43ab0888a9
Kareem Francis <francis.kareem+git@gmail.com>
2014-07-02 00:26:44 -0400

    Removed mean.io permission check from Event router

--- server/routes/eventRoutes.js
+++ server/routes/eventRoutes.js
@@ -22,8 +22,8 @@ module.exports = function(app) {
 
     app.route(baseUrl + '/:eventId')
         .get(events.show)
-        .put(authorization.requiresLogin, hasAuthorization, events.update)
-        .delete(authorization.requiresLogin, hasAuthorization, events.destroy);
+        .put(authorization.requiresLogin, events.update)
+        .delete(authorization.requiresLogin, events.destroy);
 
     app.route(baseUrl + '/:eventId/publish')
         .post(events.publish);
