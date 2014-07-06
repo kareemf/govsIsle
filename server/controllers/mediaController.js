0b15302f27d8164b1839d6efad0587389b6803e0
Kareem Francis <francis.kareem+git@gmail.com>
2014-07-06 15:24:31 -0400

    Implemented getModelType and getModelInstance methods

--- server/controllers/mediaController.js
+++ server/controllers/mediaController.js
@@ -2,14 +2,38 @@
 
 var mongoose = require('mongoose');
 
-exports.getModelType = function(req, res, next, id) {
+exports.getModelType = function(req, res, next, modelType) {
     console.log('getModelType');
+
+    var Model = mongoose.model(modelType);
+    console.log('media model', Model);
+
+    if(Model){
+        req.model = Model;
+    }
+
     next();
 };
 
 exports.getModelInstance = function(req, res, next, id) {
     console.log('getModelInstance');
-    next();
+
+    var Model = req.model;
+    if(Model){
+        Model.findOne({_id: id})
+        .exec(function(err, doc){
+            console.log('getModelInstance err:', err, 'doc:', doc);
+            if(err){
+                return res.send(500, err);
+            }
+            if(!doc){
+                return res.send(500, 'Unable to find', Model.modelName, 'with id', id);
+            }
+
+            req.doc = doc;
+            next();
+        });
+    }
 };
 
 exports.create = function(req, res) {
