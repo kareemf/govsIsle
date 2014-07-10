02110959f009bee178de8221a9461f749b739f2b
Kareem Francis <francis.kareem+git@gmail.com>
2014-07-09 23:25:56 -0400

    Media creation requires login

--- server/routes/mediaRoutes.js
+++ server/routes/mediaRoutes.js
@@ -1,12 +1,13 @@
 'use strict';
 
-var media = require('../controllers/mediaController');
+var media = require('../controllers/mediaController'),
+    authorization = require('./middlewares/authorization');
 
 module.exports = function(app) {
     var baseUrl = '/api/v1/media';
 
     app.route(baseUrl + '/:modelType/:modelId')
-        .post(media.create);
+        .post(authorization.requiresLogin, media.create);
 
     app.param('modelType', media.getModelType);
     app.param('modelId', media.getModelInstance);
