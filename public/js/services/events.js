efde88b20b2ffad1ee8b993210e32d8db56961aa
Kareem Francis <francis.kareem+git@gmail.com>
2014-07-15 23:41:57 -0400

    Updated Events service definition

--- public/js/services/events.js
+++ public/js/services/events.js
@@ -1,3 +1,7 @@
+'use strict';
+
+var services = angular.module('app.services');
+
 services.factory('Events', ['$resource', function($resource){
     var eventUrl = 'api/v1/events/:eventId';
     return $resource(eventUrl, {
