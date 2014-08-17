3b11dc1fbc8c0067b18b63942bab6853bd4c64da
Kareem Francis <francis.kareem@gmail.com>
2014-08-17 14:36:53 -0400

    Added proposed data model for recurring Events. Currently unused/unimplemented.

--- server/models/recurring.js
+++ server/models/recurring.js
@@ -1,3 +1,15 @@
-/**
- * Created by Kareem on 8/14/2014.
- */
+'use strict';
+
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema,
+    ObjectId = mongoose.Schema.ObjectId;
+
+var RecurringSchema = new Schema({
+    days: [String], //Mon-Sun
+    months: [String], //Jan-Dec
+    frequencies: [String], //1st - 4th
+    excluding: [Date], //Holidays
+    until: Date //Will not recur after
+});
+
+exports.schema = RecurringSchema;
