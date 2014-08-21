8e69515589e0876ec49cb8d817ffae8445f160e2
Kareem Francis <francis.kareem@gmail.com>
2014-08-20 20:12:27 -0400

    Fixed bug where stream data was being emitted to all users by all users, creating a multiplicative effect. Added comments

--- server/sockets/alertSocketRoutes.js
+++ server/sockets/alertSocketRoutes.js
@@ -5,22 +5,39 @@ var socketIo = require('socket.io'),
 module.exports = function(server){
     var io = socketIo.listen(server);
 
+    var num_connections = 0;
+    var previous_data = []; //TODO: cache via redis
+
+    //TODO: only published (unless admin),
+    //TODO: enddate >= today
+    //TODO: published == true
+    var stream = Alert.find().tailable().stream();
+
+    stream.on('data', function (doc) {
+        //note: there may be as few a 0 users connected at this point, meaning that noone may see this initial data
+        //hence, it is stored in previousData, so that it will be accessible to all who join later
+        console.log('Alert stream doc', doc, 'num_connections emitting to:', num_connections);
+
+        io.emit('alert', doc);
+        previous_data.push(doc);
+    });
+
+    stream.on('error', function (err) {
+        console.error('Alert stream error', err);
+    });
+
+    stream.on('close', function () {
+        console.log('Alert stream closed');
+    });
+
     io.sockets.on('connection', function(socket) {
-        console.log('user connected to socket', socket.id);
+        console.log('user connected to socket. num_connections', ++num_connections);
 
-        var stream = Alert.find().tailable().stream();
+        //connecting user may have missed out on all previous transmissions, get them up to speed
+        io.sockets.socket(socket.id).emit('alerts', previous_data);
 
-        stream.on('data', function (doc) {
-            console.log('Alert stream doc', doc);
-            io.emit('alert', doc);
-        });
-
-        stream.on('error', function (err) {
-            console.error('Alert stream error', err);
-        });
-
-        stream.on('close', function () {
-            console.log('Alert stream closed');
+        socket.on('disconnect', function(){
+            console.log('user disconnected. num_connections', --num_connections);
         });
     });
 };
