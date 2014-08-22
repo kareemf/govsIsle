c8d26ea708194550e72754bd6b410eb1a482ea4c
Kareem Francis <francis.kareem@gmail.com>
2014-08-21 22:54:44 -0400

    index.js: connection to web socket happens as part of a callback which is executed after login check. different event is emitted based on whether user is authenticated or anonymous

--- public/js/controllers/index.js
+++ public/js/controllers/index.js
@@ -4,47 +4,54 @@ var controllers = angular.module('app.controllers');
 
 controllers.controller('AppController', ['$scope', '$rootScope', '$http', 'Shared',
 function($scope, $rootScope, $http, Shared){
-    var checkLoggedIn = function(){
+    var checkLoggedIn = function(callback){
         $http.get('/loggedin')
             .success(function(response) {
                 console.log('loggedin check success. response', response);
                 if(angular.isObject(response) && response._id){
                     console.log('logged in user', response);
 
-                    $rootScope.user = response;
+                    $rootScope.user = response;                    
+                }
+                if(callback){
+                    callback($rootScope.user);
                 }
             })
             .error(function() {
                 console.log('loggedin check failure');
+                if(callback){
+                    callback(null);
+                }
             });
     };
 
-    checkLoggedIn();
+    var connectToSocket = function(user){
+        var socket = io.connect(window.location.origin);
+        Shared.alerts = [];
 
-    var socket = io.connect(window.location.origin);
-    Shared.alerts = [];
+        socket.on('connect', function(){
+            console.log('connected to web socket');
 
-    socket.on('connect', function () {
-        console.log('connected to web socket');
-        // if($rootScope.user){
-            // socket.emit('authenicated_connection', $rootScope.user);
-        // }
-        $scope.$watch(function(){return $rootScope.user && $rootScope.user.id}, function(){
-            var user = $rootScope.user;
             if(user){
                 console.log('emitting authenicated_connection to websocket');
-                socket.emit('authenicated_connection', $rootScope.user);
+                socket.emit('authenicated_connection', user);
             }
-        })
-    });
+            else{
+                console.log('emitting anonymous_connection to websocket');
+                socket.emit('anonymous_connection');
+            }
+        });
 
-    socket.on('alerts', function(alerts){
-        console.log('alerts recieved', alerts);
-        Shared.alerts = Shared.alerts.concat(alerts);
-    });
+        socket.on('alerts', function(alerts){
+            console.log('alerts recieved', alerts);
+            Shared.alerts = Shared.alerts.concat(alerts);
+        });
 
-    socket.on('alert', function(alert){
-        console.log('alert recieved', alert);
-        Shared.alerts.push(alert);
-    });
+        socket.on('alert', function(alert){
+            console.log('alert recieved', alert);
+            Shared.alerts.push(alert);
+        });
+    };
+
+    checkLoggedIn(connectToSocket);
 }]);
