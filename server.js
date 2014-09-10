d49f55caa965e7aa1d6654c02dc0975bd1777887
Kareem Francis <francis.kareem@gmail.com>
2014-09-10 02:58:17 -0400

    Discovered bug related to socket.io that was preventing index route handler from from responding to requests if it defined in a separate module. Defining it in the same module as the app var works

--- server.js
+++ server.js
@@ -35,6 +35,11 @@ function bootstrapSocketRoutes() {
 
 bootstrapSocketRoutes();
 
+app.route('/')
+    .get(function(req, res){
+        res.sendFile(__dirname + '/public/index.html');
+    });
+
 // Initializing logger
 logger.init(app, passport, mongoose);
 
